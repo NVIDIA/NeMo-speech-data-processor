f310b01fe68c1996872e3847e842b7cf005054af
Nikolay Karpov <nkarpov@nvidia.com>
2023-12-13 21:09:55 -0800

    deps
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>

--- sdp/processors/langs/armenian.py
+++ sdp/processors/langs/armenian.py
@@ -10,15 +10,15 @@ class GetSource(BaseParallelProcessor):
     Processor for extracting source information from file paths and updating the manifest.
 
     Args:
-    - source_filepath (str): The field containing the file path in the manifest.
-    - source_field (str): The field to store the extracted source information in the manifest.
-    - **kwargs: Additional keyword arguments to be passed to the base class `BaseParallelProcessor`.
+        source_filepath (str): The field containing the file path in the manifest.
+        source_field (str): The field to store the extracted source information in the manifest.
+        **kwargs: Additional keyword arguments to be passed to the base class `BaseParallelProcessor`.
 
     Methods:
-    - process_dataset_entry(data_entry): Processes a single dataset entry, extracts source information, and updates the manifest.
+        process_dataset_entry(data_entry): Processes a single dataset entry, extracts source information, and updates the manifest.
 
     Note:
-    - This class inherits from the `BaseParallelProcessor` class and extends its functionality to extract source information from file paths and update the manifest.
+        This class inherits from the `BaseParallelProcessor` class and extends its functionality to extract source information from file paths and update the manifest.
     """
     def __init__(
         self,
@@ -42,13 +42,13 @@ class MakeTsv(BaseProcessor):
     Processor for converting a JSON manifest file to a TSV (Tab-Separated Values) file.
 
     Args:
-    - **kwargs: Additional keyword arguments to be passed to the base class `BaseProcessor`.
+        **kwargs: Additional keyword arguments to be passed to the base class `BaseProcessor`.
 
     Methods:
-    - process(): Reads the input JSON manifest file, converts it to a DataFrame, and saves it as a TSV file.
+        process(): Reads the input JSON manifest file, converts it to a DataFrame, and saves it as a TSV file.
 
     Note:
-    - This class inherits from the `BaseProcessor` class and provides functionality to convert a JSON manifest file to a TSV file.
+        This class inherits from the `BaseProcessor` class and provides functionality to convert a JSON manifest file to a TSV file.
     """
     def __init__(
         self,
@@ -65,15 +65,15 @@ class RandomTsvPart(BaseProcessor):
     Processor for creating a random subset of a TSV (Tab-Separated Values) file based on the specified fraction.
 
     Args:
-    - part (float): The fraction of the dataset to include in the random subset, should be in the range (0.0, 1.0).
-    - random_state (int): Seed for reproducibility when generating the random subset.
-    - **kwargs: Additional keyword arguments to be passed to the base class `BaseProcessor`.
+        part (float): The fraction of the dataset to include in the random subset, should be in the range (0.0, 1.0).
+        random_state (int): Seed for reproducibility when generating the random subset.
+        **kwargs: Additional keyword arguments to be passed to the base class `BaseProcessor`.
 
     Methods:
-    - process(): Reads the input TSV manifest file, creates a random subset based on the specified fraction, and saves it as a new TSV file.
+        process(): Reads the input TSV manifest file, creates a random subset based on the specified fraction, and saves it as a new TSV file.
 
     Note:
-    - This class inherits from the `BaseProcessor` class and provides functionality to create a random subset of a TSV file.
+        This class inherits from the `BaseProcessor` class and provides functionality to create a random subset of a TSV file.
     """
     def __init__(
         self,
