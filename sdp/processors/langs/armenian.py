8289f822f32fad33fbd37c6326fc62125fd59934
Nikolay Karpov <nkarpov@nvidia.com>
2023-12-18 06:29:23 -0800

    header
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>

--- sdp/processors/langs/armenian.py
+++ sdp/processors/langs/armenian.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import pandas as pd
 from pathlib import Path
@@ -40,12 +54,6 @@ class MakeTsv(BaseProcessor):
         **kwargs: Additional keyword arguments to be passed to the base class `BaseProcessor`.
 
     """
-    def __init__(
-        self,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-
     def process(self):
         df1 = pd.DataFrame.from_records(load_manifest(Path(self.input_manifest_file)))
         df1.to_csv(self.output_manifest_file, index=None, sep='\t')
