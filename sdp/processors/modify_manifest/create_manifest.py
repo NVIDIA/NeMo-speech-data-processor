c9a703c64f121eab6b3802fbbb79b1fba9152e19
Nikolay Karpov <nkarpov@nvidia.com>
2024-02-29 23:21:22 -0800

    key
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>
    Signed-off-by: Ara-Yeroyan <ar23yeroyan@gmail.com>

--- sdp/processors/modify_manifest/create_manifest.py
+++ sdp/processors/modify_manifest/create_manifest.py
@@ -16,14 +16,15 @@ from pathlib import Path
 
 from sdp.processors.base_processor import BaseParallelProcessor, DataEntry
 
+
 class CreateInitialManifestByExt(BaseParallelProcessor):
     """
     Processor for creating an initial dataset manifest by saving filepaths with a common extension to the field specified in output_field.
 
     Args:
         raw_data_dir (str): The root directory of the files to be added to the initial manifest. This processor will recursively look for files with the extension 'extension' inside this directory.
-        output_field (str): The field to store the paths to the files in the dataset.
-        extension (str): The field stecify extension of the files to use them in the dataset.
+        output_file_key (str): The key to store the paths to the files in the dataset.
+        extension (str): The key to stecify extension of the files to use them in the dataset.
         **kwargs: Additional keyword arguments to be passed to the base class `BaseParallelProcessor`.
 
     """
@@ -31,21 +32,19 @@ class CreateInitialManifestByExt(BaseParallelProcessor):
     def __init__(
         self,
         raw_data_dir: str,
-        output_field: str = "audio_filepath",
+        output_file_key: str = "audio_filepath",
         extension: str = "mp3",
         **kwargs,
     ):
         super().__init__(**kwargs)
         self.raw_data_dir = Path(raw_data_dir)
-        self.output_field = output_field
+        self.output_file_key = output_file_key
         self.extension = extension
 
     def read_manifest(self):
-        input_files = [str(self.raw_data_dir / file) for file in \
-                       self.raw_data_dir.rglob('*.' + self.extension)]
-        return input_files
-    
+        output_file = [str(self.raw_data_dir / file) for file in self.raw_data_dir.rglob('*.' + self.extension)]
+        return output_file
+
     def process_dataset_entry(self, data_entry):
-        data = {self.output_field: data_entry}
+        data = {self.output_file_key: data_entry}
         return [DataEntry(data=data)]
-    
