420e18a26ba54f7049ea2b80bf4dd830a9d50e9a
George <37293288+Jorjeous@users.noreply.github.com>
2024-12-17 11:50:57 +0400

    Procc docstr example (#96)
    * Example of new processors docstring
    
    Signed-off-by: George <gzelenfroind@nvidia.com>
    
    * Example of processor docstr [new style], on DuplicateFields
    
    Signed-off-by: George <gzelenfroind@nvidia.com>
    
    ---------
    
    Signed-off-by: George <gzelenfroind@nvidia.com>

--- sdp/processors/modify_manifest/common.py
+++ sdp/processors/modify_manifest/common.py
@@ -144,9 +144,17 @@ class DuplicateFields(BaseParallelProcessor):
 
     Returns:
         The same data as in the input manifest with duplicated fields
-        as specified in the ``duplicate_fields`` input dictionary.
-    """
+        as specified in the ``duplicate_fields`` input dictionary. 
+    
+    Example:
+        .. code-block:: yaml
 
+            - _target_: sdp.processors.modify_manifest.common.DuplicateFields
+              input_manifest_file: ${workspace_dir}/test1.json
+              output_manifest_file: ${workspace_dir}/test2.json
+              duplicate_fields: {"text":"answer"}
+
+    """
     def __init__(
         self,
         duplicate_fields: Dict,
