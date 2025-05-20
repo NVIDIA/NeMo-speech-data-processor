ef4515bb1191504863b05a7af2da866ed3bbfcdc
Sasha Meister <ameister@nvidia.com>
2025-05-20 10:47:12 -0700

    Removed extra line
    Signed-off-by: Sasha Meister <ameister@nvidia.com>

--- sdp/processors/datasets/granary/create_initial_manifest.py
+++ sdp/processors/datasets/granary/create_initial_manifest.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# let's import all supported processors here to simplify target specification
-
 import os
 import json
 from glob import glob
