4a121393bad739b9971cf3e029dec3bb3524b3c3
Nune <ntadevosyan@nvidia.com>
2025-06-12 13:17:23 +0400

    Adding copyrights
    Signed-off-by: Nune <ntadevosyan@nvidia.com>

--- sdp/processors/ipl/nemo_run_processor.py
+++ sdp/processors/ipl/nemo_run_processor.py
@@ -1,4 +1,19 @@
-from sdp.processors.IPL.ipl_processors import TrainingCommandGenerator, InferenceCommandGenerator
+
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from sdp.processors.ipl.ipl_processors import TrainingCommandGenerator, InferenceCommandGenerator
 from sdp.processors.base_processor import BaseProcessor
 from omegaconf import OmegaConf, open_dict
 import os
