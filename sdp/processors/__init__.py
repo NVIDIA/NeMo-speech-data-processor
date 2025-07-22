9081c95cc1d1343a8a00fe0d2599fb98593b2d75
Nikolay Karpov <karpnv@gmail.com>
2025-07-22 16:26:49 -0700

    Portuguese (#77)
    * segment_duration
    
    Signed-off-by: Nikolay Karpov <karpnv@gmail.com>
    
    * nemo_path
    
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>
    
    * mv to unlabeled
    
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>
    
    * add docs
    
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>
    
    * fix from main
    
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>
    
    * Test check (#128)
    
    * Test check
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Update
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Small changes
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Small changes
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Small changes
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Small changes
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Small changes
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Small changes
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Ignore duration as it differs based on setup
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Bringing back tests
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * remove prints
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * remove prints
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Remove
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    ---------
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>
    
    * Doc update
    
    Signed-off-by: Nune <ntadevosyan@nvidia.com>

--- sdp/processors/__init__.py
+++ sdp/processors/__init__.py
@@ -32,9 +32,8 @@ from sdp.processors.datasets.fleurs.create_initial_manifest import (
     CreateInitialManifestFleurs,
 )
 from sdp.processors.datasets.hifitts2.download_dataset import DownloadHiFiTTS2
-from sdp.processors.datasets.hifitts2.remove_failed_chapters import RemovedFailedChapters
-from sdp.processors.datasets.uzbekvoice.create_initial_manifest import (
-    CreateInitialManifestUzbekvoice,
+from sdp.processors.datasets.hifitts2.remove_failed_chapters import (
+    RemovedFailedChapters,
 )
 from sdp.processors.datasets.ksc2.create_initial_manifest import (
     CreateInitialManifestKSC2,
@@ -44,13 +43,15 @@ from sdp.processors.datasets.librispeech.create_initial_manifest import (
     CreateInitialManifestLibrispeech,
 )
 from sdp.processors.datasets.masc import (
-    CreateInitialManifestMASC,
     AggregateSegments,
+    CreateInitialManifestMASC,
+    GetCaptionFileSegments,
     RegExpVttEntries,
-    GetCaptionFileSegments
 )
-from sdp.processors.datasets.mediaspeech.create_initial_manifest import CreateInitialManifestMediaSpeech
 from sdp.processors.datasets.mcv.create_initial_manifest import CreateInitialManifestMCV
+from sdp.processors.datasets.mediaspeech.create_initial_manifest import (
+    CreateInitialManifestMediaSpeech,
+)
 from sdp.processors.datasets.mls.create_initial_manifest import CreateInitialManifestMLS
 from sdp.processors.datasets.mls.restore_pc import RestorePCForMLS
 from sdp.processors.datasets.mtedx.create_initial_manifest import (
@@ -67,18 +68,20 @@ from sdp.processors.datasets.slr140.create_initial_manifest import (
     CreateInitialManifestSLR140,
     CustomDataSplitSLR140,
 )
+from sdp.processors.datasets.uzbekvoice.create_initial_manifest import (
+    CreateInitialManifestUzbekvoice,
+)
 from sdp.processors.datasets.voxpopuli.create_initial_manifest import (
     CreateInitialManifestVoxpopuli,
 )
 from sdp.processors.datasets.voxpopuli.normalize_from_non_pc_text import (
     NormalizeFromNonPCTextVoxpopuli,
 )
-from sdp.processors.datasets.ytc.create_initial_manifest import (
-    CreateInitialManifestYTC,
+from sdp.processors.datasets.ytc.create_initial_manifest import CreateInitialManifestYTC
+from sdp.processors.huggingface.create_initial_manifest import (
+    CreateInitialManifestHuggingFace,
 )
 from sdp.processors.huggingface.speech_recognition import ASRTransformers
-from sdp.processors.huggingface.create_initial_manifest import CreateInitialManifestHuggingFace
-
 from sdp.processors.modify_manifest.common import (
     AddConstantFields,
     ApplyInnerJoin,
@@ -89,7 +92,9 @@ from sdp.processors.modify_manifest.common import (
     RenameFields,
     SortManifest,
     SplitOnFixedDuration,
+    Subprocess,
     DropSpecifiedFields,
+
 )
 from sdp.processors.modify_manifest.create_manifest import (
     CreateCombinedManifests,
@@ -104,8 +109,8 @@ from sdp.processors.modify_manifest.data_to_data import (
     GetWER,
     InsIfASRInsertion,
     InverseNormalizeText,
-    NormalizeText,
     MakeSentence,
+    NormalizeText,
     ReadDocxLines,
     ReadTxtLines,
     SplitLineBySentence,
@@ -130,8 +135,8 @@ from sdp.processors.modify_manifest.data_to_dropbool import (
     DropLowWordMatchRate,
     DropNonAlphabet,
     DropOnAttribute,
-    PreserveByValue,
     DropRepeatedFields,
+    PreserveByValue,
 )
 from sdp.processors.modify_manifest.make_letters_uppercase_after_period import (
     MakeLettersUppercaseAfterPeriod,
@@ -148,6 +153,7 @@ from sdp.processors.manage_files.remove import (
 )
 from sdp.processors.nemo.asr_inference import ASRInference
 from sdp.processors.nemo.estimate_bandwidth import EstimateBandwidth
+from sdp.processors.nemo.lid_inference import AudioLid
 from sdp.processors.nemo.pc_inference import PCInference
 from sdp.processors.toloka.accept_if import AcceptIfWERLess
 from sdp.processors.toloka.create_pool import CreateTolokaPool
